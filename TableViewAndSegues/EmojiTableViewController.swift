71692cfc0c097bf0e1c0a0488a23558de82d4ebd
Anıl Can <anillcan7@gmail.com>
2021-04-14 22:47:16 +0300

    Table View added and simple array listed on cells

---
+++ TableViewAndSegues/EmojiTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  EmojiTableViewController.swift
+//  TableViewAndSegues
+//
+//  Created by Anil Can on 14.04.2021.
+//
+
+import UIKit
+
+class EmojiTableViewController: UITableViewController {
+
+    let emojiArray = ["🏎", "⛪️", "😦"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    // MARK: - How many rows?
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return emojiArray.count
+    }
+
+    
+    // MARK: - What goes in each row?
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+
+        // Configure the cell...
+        
+        cell.textLabel?.text = emojiArray[indexPath.row]
+
+        return cell
+    }
+
+}
